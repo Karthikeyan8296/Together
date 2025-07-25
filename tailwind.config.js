6fc4c7004faf13a0de3ea88685a3bfc270a4466c
Karthikeyan8296 <karthikeyan76652@gmail.com>
2025-07-25 09:12:15 +0530

    fonts imported for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["./index.ts", "./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter_thin: ["Inter_18pt-Thin", "SpaceMono-Regular"],
+        Inter_extraLight: ["Inter_18pt-ExtraLight", "SpaceMono-Regular"],
+        Inter_light: ["Inter_18pt-Light", "SpaceMono-Regular"],
+        Inter_regular: ["Inter_18pt-Regular", "SpaceMono-Regular"],
+        Inter_medium: ["Inter_18pt-Medium", "SpaceMono-Regular"],
+        Inter_semiBold: ["Inter_18pt-SemiBold", "SpaceMono-Regular"],
+        Inter_bold: ["Inter_18pt-Bold", "SpaceMono-Regular"],
+        Inter_extraBold: ["Inter_18pt-ExtraBold", "SpaceMono-Regular"],
+        Inter_black: ["Inter_18pt-Black", "SpaceMono-Regular"],
+      },
+    },
   },
   plugins: [],
 };
