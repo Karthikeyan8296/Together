3fbe4800588c277a7419c37ae4a8c11ee9f44861
Karthikeyan8296 <karthikeyan76652@gmail.com>
2025-11-18 21:56:41 +0530

    color and typo imported

--- app/src/main/java/com/example/together/MainActivity.kt
+++ app/src/main/java/com/example/together/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.together
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
@@ -13,13 +15,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.together.ui.theme.Inter
 import com.example.together.ui.theme.TogetherTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT, //background
+                Color.BLACK //foreground
+            )
+        )
         setContent {
             TogetherTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -30,7 +41,12 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Text("Welcome to Together")
+                        Text(
+                            text = "Welcome to Together",
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 24.sp,
+                            letterSpacing = 0.sp
+                        )
                     }
                 }
             }
--- app/src/main/java/com/example/together/ui/theme/Type.kt
+++ app/src/main/java/com/example/together/ui/theme/Type.kt
@@ -2,33 +2,48 @@ package com.example.together.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.together.R
+
+
+// Inter font family with all weights
+val Inter = FontFamily(
+    Font(R.font.inter_18pt_thin, FontWeight.Thin),
+    Font(R.font.inter_18pt_extralight, FontWeight.ExtraLight),
+    Font(R.font.inter_18pt_light, FontWeight.Light),
+    Font(R.font.inter_18pt_regular, FontWeight.Normal),
+    Font(R.font.inter_18pt_medium, FontWeight.Medium),
+    Font(R.font.inter_18pt_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_18pt_bold, FontWeight.Bold),
+    Font(R.font.inter_18pt_extrabold, FontWeight.ExtraBold),
+    Font(R.font.inter_18pt_black, FontWeight.Black),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = Inter,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
+    // You can override more styles later (headline, bodyMedium, etc.)
 )
